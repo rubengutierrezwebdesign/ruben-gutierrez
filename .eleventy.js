5147f0a2fadf968a65c563a60004c23224a72923
unknown <rubengutierrezwebdesign@gmail.com>
2024-11-27 16:50:08 -0800

    '1'

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/css');
     eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addPassthroughCopy('./src/pages/web-designer');
+    eleventyConfig.addPassthroughCopy('./src/pages/projects/web-designer');
 
     eleventyConfig.addCollection("posts", function(collection) {
         const coll = collection.getFilteredByTag("posts");
