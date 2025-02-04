475f83d51295595d7fdd81608d26c3ed330ee7f7
rubengutierrezwebdesign <rubengutierrezwebdesign@gmail.com>
2025-02-03 17:59:32 -0800

    1

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,17 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/pages/projects/web-designer');
     eleventyConfig.addPassthroughCopy('./src/pages/projects');
+    
+    eleventyConfig.addNunjucksFilter("postDate", function(value) {
+        // Assuming value is a Date object or a string that can be parsed into a Date
+        const date = new Date(value);
+        const options = { 
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric' 
+            };
+        return date.toLocaleDateString('en-US', options);  // Adjust format as needed
+      });
 
     eleventyConfig.addCollection("posts", function(collection) {
         const coll = collection.getFilteredByTag("posts");
@@ -25,5 +36,7 @@ module.exports = function (eleventyConfig) {
             output: "_site"
         },
     }
+
 }
+
 
