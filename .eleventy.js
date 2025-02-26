6d43c1e0b87f1a9c801f60b5ffd0fd519fb7d5da
rubengutierrezwebdesign <rubengutierrezwebdesign@gmail.com>
2025-02-25 20:15:16 -0800

    1

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addPassthroughCopy('./src/pages/projects/');
+    eleventyConfig.addPassthroughCopy('./src/pages/');
 
     
     eleventyConfig.addNunjucksFilter("postDate", function(value) {
