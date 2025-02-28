d5e6475e8e43b30fd4cfad5c296bb061c3509e63
rubengutierrezwebdesign <rubengutierrezwebdesign@gmail.com>
2025-02-28 14:45:11 -0800

    1

---
+++ _site/assets/js/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCZCQ6uQSw1NYF1SeCe_cDqQSgjL3xkMU",
+    authDomain: "contact-form-c8fef.firebaseapp.com",
+    projectId: "contact-form-c8fef",
+    storageBucket: "contact-form-c8fef.firebasestorage.app",
+    messagingSenderId: "946172510136",
+    appId: "1:946172510136:web:f9f52263ad7546ff4a994d"
+};
+
